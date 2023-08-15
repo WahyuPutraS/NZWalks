be7a33dd99069b9fc4432404aaecd003f8d56399
WahyuPutraS <wahyusuartama2002@gmail.com>
2023-08-15 15:00:05 +0800

    End Of Section 5

--- NZWalks/NZWalks.API/Models/DTO/Walk.cs
+++ NZWalks/NZWalks.API/Models/DTO/Walk.cs
@@ -1,4 +1,6 @@
-﻿namespace NZWalks.API.Models.Domain
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Models.DTO
 {
     public class Walk
     {
@@ -14,6 +16,5 @@
         // Navigation Property
         public Region Region { get; set; }
         public WalkDifficulty WalkDifficulty { get; set; }
-
     }
 }
